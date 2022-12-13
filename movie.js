bf31913dc203f5d5892a16d84cf3976e010248e1
Diogo Santos <97601965+diogosec@users.noreply.github.com>
2022-12-13 15:19:10 +0000

    Initial commit

---
+++ movie.js
@@ -0,0 +1,16 @@
+var main = document.querySelectorAll('.main .center') [0];
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=506fadb0256c13349acc05dabebf9604&language=en-US&page=1')
+.then(response => response.json())
+.then(data =>{
+  data.results.map((i, j)=>{
+    if(j > 10)
+      return;
+    if(j == 0){
+      main.innerHTML+=`
+
+        <div class ="banner-principal" style="background-image-url('https://image.tmdb.org/t/p/original`+i.backdrop_path+`')"></div>
+        `;
+        
+    }
+  });
+})
